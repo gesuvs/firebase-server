c57852a9f236e1bf42b9cc765e8c4bc43b96fd29
GBernardo10 <g_bernardo10@hotmail.com>
2019-12-19 15:56:56 -0300

    config environment variables

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
+require('dotenv/config');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const algoliasearch = require('algoliasearch');
-
-const ALGOLIA_ID = 'MWVBKORLTJ';
-const ALGOLIA_ADMIN_KEY = '7d254f5280676cfa5e90f5a28ab61b60';
-const ALGOLIA_INDEX_NAME = 'eventos';
+const ALGOLIA_ID = process.env.ALGOLIA_ID;
+const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY;
+const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
 
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 admin.initializeApp(functions.config().firebase);
@@ -21,7 +21,7 @@ exports.onProductCreated = functions.firestore
 
 exports.onProductUpdated = functions.firestore
     .document('eventos/eventosID')
-    .onUpdate(async (change, context)=>{
+    .onUpdate(async (change, context) => {
       const evento = change.after.data();
       evento.objectID = context.params.eventosID;
 
@@ -31,7 +31,7 @@ exports.onProductUpdated = functions.firestore
 
 exports.onProductDeleted = functions.firestore
     .document('eventos/eventosID')
-    .onDelete((snap)=>{
+    .onDelete((snap) => {
       const index = client.initIndex(ALGOLIA_INDEX_NAME);
       return index.deleteObject(snap.id);
     });
