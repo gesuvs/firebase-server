9c7dbed6888780e8f23a64ab09ce9da3371d67f9
GBernardo10 <g_bernardo10@hotmail.com>
2019-12-22 02:54:20 -0300

    fix

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
-require('dotenv/config');
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const algoliasearch = require('algoliasearch');
+require("dotenv").config({
+  path: __dirname + "/../.env"
+});
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const algoliasearch = require("algoliasearch");
 const ALGOLIA_ID = process.env.ALGOLIA_ID;
 const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY;
 const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
@@ -10,31 +12,31 @@ const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 admin.initializeApp(functions.config().firebase);
 
 exports.onProductCreated = functions.firestore
-    .document('eventos/{eventosID}')
-    .onCreate((snap, context) => {
-      const evento = snap.data();
+  .document("eventos/{eventosID}")
+  .onCreate((snap, context) => {
+    const evento = snap.data();
 
-      evento.objectID = context.params.eventosID;
-      const index = client.initIndex(ALGOLIA_INDEX_NAME);
-      return index.saveObject(evento);
-    });
+    evento.objectID = context.params.eventosID;
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.saveObject(evento);
+  });
 
 exports.onProductUpdated = functions.firestore
-    .document('eventos/eventosID')
-    .onUpdate(async (change, context) => {
-      const evento = change.after.data();
-      evento.objectID = context.params.eventosID;
+  .document("eventos/eventosID")
+  .onUpdate(async (change, context) => {
+    const evento = change.after.data();
+    evento.objectID = context.params.eventosID;
 
-      const index = client.initIndex(ALGOLIA_INDEX_NAME);
-      await index.partialUpdateObject(evento);
-    });
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    await index.partialUpdateObject(evento);
+  });
 
 exports.onProductDeleted = functions.firestore
-    .document('eventos/eventosID')
-    .onDelete((snap) => {
-      const index = client.initIndex(ALGOLIA_INDEX_NAME);
-      return index.deleteObject(snap.id);
-    });
+  .document("eventos/eventosID")
+  .onDelete(snap => {
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.deleteObject(snap.id);
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
